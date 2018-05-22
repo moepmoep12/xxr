1b6ed91e861dc1720ce7b46f0cfeac443270bfe6
masaka <masaka@mz-kb.com>
2018-05-23 04:54:47 +0900

    Add MatchSet constructor TODO: the latter part of GENERATE MATCH SET

--- XCS/match_set.h
+++ XCS/match_set.h
@@ -1,10 +1,28 @@
 #pragma once
 
 #include "symbol.h"
+#include "population.h"
+#include "state.h"
+#include "classifier.h"
 #include "classifier_repository.h"
 
 template <class S = BinarySymbol, class Action = std::string>
 class MatchSet : public ClassifierRepository<S, Action>
 {
+private:
+    using ClassifierRepository<S, Action>::m_actionCount;
+    
 public:
+    MatchSet(Population<S, Action> & population, const State<S> & situation)
+    {
+        for (auto && cl : population.classifiers())
+        {
+            if (cl.condition.contains(situation))
+            {
+                this->addClassifier(cl);
+            }
+        }
+
+        /* TODO: the latter part of GENERATE MATCH SET */
+    }
 };
