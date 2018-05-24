0b822229741cb08c51182f7c3127d4ca04608c9c
masaka <masaka@mz-kb.com>
2018-05-25 01:08:15 +0900

    Add covering classifier generation

--- XCS/xcs.h
+++ XCS/xcs.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <list>
 #include <unordered_set>
+#include <cstdint>
 
 #include "xcs_constants.h"
 #include "symbol.h"
@@ -19,28 +20,44 @@ protected:
 
 	std::unordered_set<Action> m_actionChoices;
 
-	void generateMatchList(std::list<Classifier<S, Action>> & matchList, const State<S> & situation)
-	{
-		matchList.clear();
+	uint64_t m_timeStamp;
 
-		auto availableActionChoices = m_actionChoices;
+	void generateMatchSet(std::list<Classifier<S, Action>> & matchSet, const State<S> & situation)
+	{
+		matchSet.clear();
+
+		auto unselectedActions = m_actionChoices;
 
 		for (auto && cl : m_population)
 		{
 			if (cl.condition.contains(situation))
 			{
-				matchList.push_back(cl);
-				availableActionChoices.erase(cl.action);
+				matchSet.push_back(cl);
+				unselectedActions.erase(cl.action);
 			}
 		}
 
-		// Covering
-		for (Action action : availableActionChoices)
+		// Generate classifiers covering unselected actions
+		for (Action action : unselectedActions)
 		{
-
+			auto coveringClassifier = generateCoveringClassifier(situation, action);
+			m_population.push_back(coveringClassifier);
+			// TODO: DEFETE FROM POPULATION [P]
+			matchSet.push_back(coveringClassifier);
 		}
 	}
 
+	auto generateCoveringClassifier(const State<S> & situation, Action action)
+	{
+		// Generate more general condition than given situation
+		auto condition = situation;
+		condition.randomGeneralize(m_constants.generalizeProbability);
+
+		// Generate classifier
+		Classifier<S, Action> cl(condition, action, m_timeStamp);
+		return cl;
+	}
+
 public:
-	explicit XCS(const XCSConstants & constants, const std::set<Action> & actionChoices) : m_constants(constants), m_actionChoices(actionChoices) {}
+	explicit XCS(const XCSConstants & constants, const std::set<Action> & actionChoices) : m_constants(constants), m_actionChoices(actionChoices), m_timeStamp(0) {}
 };
