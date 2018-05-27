d66448572d371118bc5897a3542fb31c28f87b66
masaka <masaka@mz-kb.com>
2018-05-28 00:06:33 +0900

    Add action set subsumption to updateSet()

--- XCS/xcs.h
+++ XCS/xcs.h
@@ -201,7 +201,10 @@ protected:
 
         updateFitness();
 
-        // TODO: action set subsumption
+        if (m_constants.doActionSetSubsumption)
+        {
+            doActionSetSubsumption();
+        }
     }
 
     void updateFitness()
