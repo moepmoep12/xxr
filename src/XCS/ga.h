9f422a2969d115d5f9e1389e4018c4f1dff1d12c
masaka <masaka@mz-kb.com>
2018-12-22 00:46:06 +0900

    Receive shared_ptr in Population::insertOrIncrementNumerosity

--- src/XCS/ga.h
+++ src/XCS/ga.h
@@ -155,7 +155,7 @@ namespace XCS
                     }
                     else
                     {
-                        population.insertOrIncrementNumerosity(*child);
+                        population.insertOrIncrementNumerosity(std::make_shared<Classifier>(*child));
                     }
                 }
                 else
