5492d6b06a5ab7be194ac7e76c400129bf7b1b1e
masaka <masaka@mz-kb.com>
2019-05-15 04:20:12 +0900

    Add readPopulation function to CSV

--- include/xxr/helper/csv.hpp
+++ include/xxr/helper/csv.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <istream>
 #include <sstream>
+#include <fstream>
 #include <vector>
 #include <cmath>
+#include <cstddef>
 
 #include "dataset.hpp"
+#include "xxr/xcs/classifier.hpp"
 
 namespace xxr
 {
@@ -54,6 +57,114 @@ namespace xxr
         {
             std::ifstream ifs(filename);
             return readDataset<T, Action>(ifs, rounds);
+        }
+
+        template <class Classifier>
+        std::vector<Classifier> readPopulation(std::istream & is, bool skipFirstLine = true, bool skipFirstColumn = false)
+        {
+            using T = typename Classifier::type;
+            using Action = typename Classifier::ActionType;
+
+            std::vector<Classifier> classifiers;
+
+            std::string line;
+
+            if (skipFirstLine)
+            {
+                std::getline(is, line);
+            }
+
+            // Load all lines from csv
+            while (std::getline(is, line) && !line.empty())
+            {
+                // Split comma-separated string
+                std::istringstream iss(line);
+                std::string field;
+                double fieldValue;
+                std::vector<T> situation;
+                bool empty = true;
+
+                // For XCSR population CSV, we need an option whether to skip the first graphic notation column
+                if (skipFirstColumn)
+                {
+                    std::getline(iss, field, ',');
+                }
+
+                using Condition = typename Classifier::ConditionType;
+                Condition condition;
+                Action action;
+                double prediction;
+                double epsilon;
+                double fitness;
+                std::size_t experience;
+                std::size_t timeStamp;
+                double actionSetSize;
+                std::size_t numerosity;
+
+                int columnIdx = 0;
+                while (std::getline(iss, field, ','))
+                {
+                    if (columnIdx == 0) // Condition
+                    {
+                        condition = Condition(field);
+                    }
+                    else
+                    {
+                        std::istringstream fieldIss(field);
+                        switch (columnIdx)
+                        {
+                        case 1: // Action
+                            fieldIss >> action;
+                            break;
+                        case 2: // Prediction
+                            fieldIss >> prediction;
+                            break;
+                        case 3: // Epsilon
+                            fieldIss >> epsilon;
+                            break;
+                        case 4: // Fitness
+                            fieldIss >> fitness;
+                            break;
+                        case 5: // Experience
+                            fieldIss >> experience;
+                            break;
+                        case 6: // TimeStamp
+                            fieldIss >> timeStamp;
+                            break;
+                        case 7: // ActionSetSize
+                            fieldIss >> actionSetSize;
+                            break;
+                        case 8: // Numerosity
+                            fieldIss >> numerosity;
+                            break;
+                        }
+                    }
+                    ++columnIdx;
+                    empty = false;
+                }
+
+                if (empty)
+                {
+                    break; // this gets rid of maybe-uninitialized warning
+                }
+
+                // Add classifier to list
+                Classifier cl(condition, action, prediction, epsilon, fitness);
+                cl.experience = experience;
+                cl.timeStamp = timeStamp;
+                cl.actionSetSize = actionSetSize;
+                cl.numerosity = numerosity;
+                classifiers.push_back(std::move(cl));
+            }
+
+            return classifiers;
+        }
+
+        template <class Classifier>
+        std::vector<Classifier> readPopulation(const std::string & filename, bool skipFirstLine = true, bool skipFirstColumn = false)
+        {
+            std::ifstream ifs(filename);
+            return readPopulation<Classifier>(ifs, skipFirstLine, skipFirstColumn);
         }
     }
 
